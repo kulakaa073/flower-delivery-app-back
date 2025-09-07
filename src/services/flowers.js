5b37cec4c00c1a4e87bae59be04a59c266878f37
Shade <kulakaa073@gmail.com>
2025-09-07 22:40:12 +0300

    update
    fixing behavior to return updated doc

--- src/services/flowers.js
+++ src/services/flowers.js
@@ -1,9 +1,11 @@
 import { UsersCollection } from '../db/models/user.js';
 
 export const updateFavourites = async (userId, favourites) => {
-  const updatedFavourites = await UsersCollection.findByIdAndUpdate(userId, {
-    favourites: favourites,
-  });
+  const updatedFavourites = await UsersCollection.findByIdAndUpdate(
+    userId,
+    { favourites },
+    { new: true },
+  );
 
   return updatedFavourites;
 };
